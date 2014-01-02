54e83b727dcac50f5774bab816cee613aec7a860
Rick <rick@rick-Aspire-5733.(none)>
2014-01-01 20:30:25 -0600

    add kids

--- LineDraw.py
+++ LineDraw.py
@@ -7,6 +7,22 @@ line_bends = 1
 
 import random
 
+class Antagonist(pygame.sprite.Sprite):
+
+    def __init__(self, image_file, location, speed):
+        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.speed = speed
+
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
+            self.speed[0] = -self.speed[0]
+        if self.rect.top < 0 or self.rect.bottom > SCREEN_DEPTH:
+            self.speed[1] = -self.speed[1]
+
 
 
 class MyMonkeyClass(pygame.sprite.Sprite):
@@ -48,6 +64,16 @@ screen.fill([255, 255, 255])
 charter = Course_charter (SCREEN_DEPTH, SCREEN_WIDTH, lane_width, 350, 300)
 pts = charter.make_line()
 monkey = MyMonkeyClass("monkey1.jpeg", pts[0])
+antagonists = []
+antagonist = Antagonist("JonahsHead.png",[10,100], [20,20])
+antagonist2 = Antagonist("LukesHead.png",[50,200], [50,40])
+antagonist3 = Antagonist("SarahsHead.png",[300,300], [-10,20])
+
+
+antagonists.append(antagonist)
+antagonists.append(antagonist2)
+antagonists.append(antagonist3)
+
 
 
 
@@ -61,6 +87,11 @@ while True:
             sys.exit()
 
     pygame.time.delay(200)
+    screen.fill([255, 255, 255])
     pygame.draw.lines(screen, [255,0,0],False, pts, lane_width)
     screen.blit(monkey.image, monkey.rect)  # 25 is about half of monkey
+    for antagonist in antagonists:
+        antagonist.move()
+        screen.blit(antagonist.image, antagonist.rect)
+
     pygame.display.flip()
